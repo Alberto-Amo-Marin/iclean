4fb1b78e486b037f3a7115dff8446101ba67f8d6
aamomari <alberto.amo-marin@capgemini.com>
2018-07-20 20:10:17 +0200

    [FIX] slide-screen

--- src/app/typeOfTask/type-of-task/type-of-task.component.ts
+++ src/app/typeOfTask/type-of-task/type-of-task.component.ts
@@ -5,11 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './type-of-task.component.html',
   styleUrls: ['./type-of-task.component.scss']
 })
-export class TypeOfTaskComponent implements OnInit {
 
+export class TypeOfTaskComponent implements OnInit {
+  
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
